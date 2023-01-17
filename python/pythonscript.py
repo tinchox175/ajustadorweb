59eac245caf964dc2236758583d13e6b510b6e52
Tincho <jerikho175@hotmail.com>
2023-01-17 17:33:40 -0300

    Update pythonscript.py

--- python/pythonscript.py
+++ python/pythonscript.py
@@ -157,7 +157,7 @@ async def nib(event):
     p_json = json.dumps(json_item(p))
     Bokeh.embed.embed_item(JSON.parse(p_json), "plot")
     document.getElementById("parametros-output").innerHTML = 'No se pudo ajustar tus datos, proba mejorando la función o los parámetros de búsqueda.'
-    break
+    return
   p.line(data1, data2, line_width = 1)
   p.circle(data1, data2)
   p_json = json.dumps(json_item(p))
