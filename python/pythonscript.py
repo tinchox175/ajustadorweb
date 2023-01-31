94a94ea771f58371cd481298ec1e4d4aa31ba02c
Tincho <jerikho175@hotmail.com>
2023-01-31 18:03:20 -0300

    Update pythonscript.py

--- python/pythonscript.py
+++ python/pythonscript.py
@@ -1,7 +1,6 @@
 import json
 from js import document, FileReader, console, JSON, Bokeh
 from pyodide import create_proxy
-from numpy import *
 import numpy as np
 from bokeh.embed import json_item
 from bokeh.plotting import figure
